ddd1cea7bf14a9feda5c327da9623fa428cc4f9f
boypete1 <soulfaster@hotmail.com>
2018-06-25 12:00:04 +0700

    Initial commit

---
+++ app/src/main/java/com/androidcai/administrator/piggymath/Answer_view.java
@@ -0,0 +1,13 @@
+package com.androidcai.administrator.piggymath;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Answer_view extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer_view);
+    }
+}
---
+++ app/src/main/java/com/androidcai/administrator/piggymath/MyConstant.java
@@ -0,0 +1,186 @@
+package com.androidcai.administrator.piggymath;
+
+public class MyConstant {
+
+    private int[] modeGame0Ints = new int[]{R.drawable.imagess_02, R.drawable.imagess_05, R.drawable.imagess_07, R.drawable.imagess_10, R.drawable.imagess_12,
+            R.drawable.imagess_15, R.drawable.imagess_17, R.drawable.imagess_18, R.drawable.imagess_21,
+            R.drawable.imagess_23, R.drawable.imagess_25, R.drawable.imagess_29, R.drawable.minute_02,
+            R.drawable.minute_05, R.drawable.minute_07, R.drawable.minute_10, R.drawable.minute_12,
+            R.drawable.minute_15, R.drawable.minute_17, R.drawable.minute_18, R.drawable.minute_21, R.drawable.minute_23};
+
+
+    private int[] modeGame1Ints = new int[]{R.drawable.imagess_02, R.drawable.imagess_05, R.drawable.imagess_07, R.drawable.imagess_10, R.drawable.imagess_12,
+            R.drawable.imagess_15, R.drawable.imagess_17, R.drawable.imagess_18, R.drawable.imagess_21,
+            R.drawable.imagess_23, R.drawable.imagess_25, R.drawable.imagess_29, R.drawable.minute_02,
+            R.drawable.minute_05, R.drawable.minute_07, R.drawable.minute_10, R.drawable.minute_12,
+            R.drawable.minute_15, R.drawable.minute_17, R.drawable.minute_18, R.drawable.minute_21, R.drawable.minute_23};
+
+    private int[] modeGame2Ints = new int[]{R.drawable.sc1,
+            R.drawable.sc_021,  R.drawable.sc_012,
+            R.drawable.sc_031, R.drawable.sc_041, R.drawable.sc_013, R.drawable.sc_023,
+            R.drawable.sc_033, R.drawable.sc_043, R.drawable.sc_014, R.drawable.sc_024,
+            R.drawable.sc_034, R.drawable.sc_044, R.drawable.sc_015, R.drawable.sc_025, R.drawable.sc_035};
+
+//    private int[] modeGame2Ints = new int[]{R.drawable.sc1, R.drawable.sc_01, R.drawable.sc_02, R.drawable.sc_03, R.drawable.sc_04,
+//            R.drawable.sc_021, R.drawable.scc1_03, R.drawable.scc1_05, R.drawable.sc_012,
+//            R.drawable.sc_031, R.drawable.sc_041, R.drawable.sc_013, R.drawable.sc_023,
+//            R.drawable.sc_033, R.drawable.sc_043, R.drawable.sc_014, R.drawable.sc_024,
+//            R.drawable.sc_034, R.drawable.sc_044, R.drawable.sc_015, R.drawable.sc_025, R.drawable.sc_035};
+
+    private int[] modeGame3Ints = new int[]{R.drawable.mr1_01, R.drawable.mr1_02, R.drawable.mr1_03, R.drawable.mr1_04, R.drawable.mr1_05,
+            R.drawable.mr1_06, R.drawable.mr7, R.drawable.mr8, R.drawable.mr9,
+            R.drawable.mr10, R.drawable.mr11, R.drawable.mr12, R.drawable.mr13,
+            R.drawable.mr14, R.drawable.mr15, R.drawable.mr16, R.drawable.mr17,
+            R.drawable.mr18, R.drawable.mr19, R.drawable.mr20, R.drawable.mr21 };
+
+    private String[] choice1Mode0Strings = new String[]{"แปดนาฬิกาสิบห้านาที", "แปดนาฬิกายี่สิบนาที", "สิบสามนาฬิกาห้าสิบห้านาที", "สิบแปดนาฬิกาสามสิบนาที", "สิบหกนาฬิกาสิบห้านาที", "หกนาฬิกาสามสิบห้านาที", "เจ็ดนาฬิกาสี่สิบห้านาที", "สิบเจ็ดนาฬิกายี่สิบนาที", "ยี่สิบนาฬิกาสิบห้านาที", "สิบห้านาฬิกาห้าสิบนาที", "สิบสองนาฬิกาสิบนาที", "สิบสามนาฬิกาห้าสิบนาที", "ยี่สิบสองนาฬิกาสิบห้านาที", "สิบเก้านาฬิกายี่สิบนาที", "สิบสี่นาฬิกาห้าสิบห้านาที", "สี่นาฬิกาสิบห้านาที", "เจ็ดนาฬิกาสามสิบนาที", "สิบเก้านาฬิกาสามสิบห้านาที", "สิบเจ็ดนาฬิกาสี่สิบห้านาที", "ห้านาฬิกาสี่สิบห้านาที", "เจ็ดนาฬิกาห้านาที"};
+    private String[] choice2Mode0Strings = new String[]{"เจ็ดนาฬิกาสิบห้านาที", "แปดนาฬิกาห้านาที", "สิบสี่นาฬิกาห้าสิบห้านาที", "สิบเก้านาฬิกาสามสิบนาที", "สิบห้านาฬิกาสิบห้านาที", "สี่นาฬิกาสามสิบห้านาที", "หกนาฬิกาสี่สิบห้านาที", "สิบเก้านาฬิกาสี่สิบนาที", "สิบเก้านาฬิกาห้านาที", "สิบแปดนาฬิกาห้าสิบนาที", "สิบเอ็ดนาฬิกาสิบนาที", "สิบสองนาฬิกาห้าสิบนาที", "ยี่สิบนาฬิกาสิบห้านาที", "ยี่สิบสองนาฬิกายี่สิบนาที", "สิบสองนาฬิกาห้าสิบห้านาที", "ห้านาฬิกาสิบห้านาที", "หกนาฬิกาสามสิบนาที", "สิบแปดนาฬิกาสามสิบห้านาที", "สิบเก้านาฬิกาสามสิบห้านาที", "สีี่นาฬิกาสี่สิบนาที", "แปดนาฬิกาห้านาที"};
+    private String[] choice3Mode0Strings = new String[]{"เก้านาฬิกาสิบห้านาที", "แปดนาฬิกาห้านาที", "สิบห้านาฬิกาห้าสิบห้านาที", "สิบเก้านาฬิกาสามสิบนาที", "สิบเจ็ดนาฬิกาสิบห้านาที", "สี่นาฬิกาสามสิบนาที", "แปดนาฬิกาสี่สิบห้านาที", "สิบเแปดนาฬิกาสี่สิบนาที", "ยี่สิบเอ็ดนาฬิกาห้านาที", "สิบเจ็ดนาฬิกาห้าสิบนาที", "เก้านาฬิกาสิบนาที", "สิบนาฬิกาห้าสิบนาที", "ยี่สิบเอ็ดนาฬิกาสิบห้านาที", "ยี่สิบนาฬิกายี่สิบนาที", "สิบสามนาฬิกาห้าสิบห้านาที", "หกนาฬิกาสิบห้านาที", "แปดนาฬิกาสามสิบนาที", "สิบเหกนาฬิกาสามสิบห้านาที", "สิบเก้านาฬิกาสี่สิบห้านาที", "หกนาฬิกาสี่สิบนาที", "หกนาฬิกาห้านาที"};
+    private String[] choice4Mode0Strings = new String[]{"แปดนาฬิกาห้านาที", "เจ็ดนาฬิกาสิบห้านาที", "สิบสองนาฬิกาห้าสิบห้านาที", "สิบเจ็ดนาฬิกาสามสิบนาที", "สิบห้านาฬิกายี่สิบนาที", "ห้านาฬิกาสามสิบห้านาที", "เจ็ดนาฬิกาสี่สิบห้านาที", "สิบเจ็ดนาฬิกาสี่สิบนาที", "ยี่สิบนาฬิกาห้านาที", "สิบหกนาฬิกาห้าสิบนาที", "สิบนาฬิกาสิบนาที", "สิบเอ็ดนาฬิกาห้าสิบนาที", "สิบเก้านาฬิกาสิบห้านาที", "ยี่สิบเอ็ดนาฬิกายี่สิบนาที", "สิบห้านาฬิกาห้าสิบห้านาที", "หกนาฬิกายี่สิบนาที", "ห้านาฬิกาสามสิบนาที", "สิบเจ็ดนาฬิกาสามสิบห้านาที", "สิบแปดนาฬิกาสี่สิบห้านาที", "ห้านาฬิกาสี่สิบนาที", "เก้านาฬิกาห้านาที"};
+
+    private int[] answerMode0Ints = new int[]{1, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 3, 2, 3, 3, 1, 2, 4, 3, 4, 2};
+    private int[] answerMode1Ints = new int[]{1, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 3, 2, 3, 3, 1, 2, 4, 3, 4, 2};
+    private int[] answerMode2Ints = new int[]{1, 2, 1, 3, 1, 2, 4, 2, 4, 3, 4, 3, 2, 3, 3, 1, 2, 4, 3, 4, 2,1};
+    private int[] answerMode3Ints = new int[]{1, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 3, 2, 3, 3, 1, 2, 4, 3, 4, 2};
+
+    private String[] choice1Mode1Strings = new String[]{"แปดนาฬิกาสิบห้านาที", "แปดนาฬิกายี่สิบนาที", "สิบสามนาฬิกาห้าสิบห้านาที", "สิบแปดนาฬิกาสามสิบนาที", "สิบหกนาฬิกาสิบห้านาที", "หกนาฬิกาสามสิบห้านาที", "เจ็ดนาฬิกาสี่สิบห้านาที", "สิบเจ็ดนาฬิกายี่สิบนาที", "ยี่สิบนาฬิกาสิบห้านาที", "สิบห้านาฬิกาห้าสิบนาที", "สิบสองนาฬิกาสิบนาที", "สิบสามนาฬิกาห้าสิบนาที", "ยี่สิบสองนาฬิกาสิบห้านาที", "สิบเก้านาฬิกายี่สิบนาที", "สิบสี่นาฬิกาห้าสิบห้านาที", "สี่นาฬิกาสิบห้านาที", "เจ็ดนาฬิกาสามสิบนาที", "สิบเก้านาฬิกาสามสิบห้านาที", "สิบเจ็ดนาฬิกาสี่สิบห้านาที", "ห้านาฬิกาสี่สิบห้านาที", "เจ็ดนาฬิกาห้านาที"};
+    private String[] choice2Mode1Strings = new String[]{"เจ็ดนาฬิกาสิบห้านาที", "แปดนาฬิกาห้านาที", "สิบสี่นาฬิกาห้าสิบห้านาที", "สิบเก้านาฬิกาสามสิบนาที", "สิบห้านาฬิกาสิบห้านาที", "สี่นาฬิกาสามสิบห้านาที", "หกนาฬิกาสี่สิบห้านาที", "สิบเก้านาฬิกาสี่สิบนาที", "สิบเก้านาฬิกาห้านาที", "สิบแปดนาฬิกาห้าสิบนาที", "สิบเอ็ดนาฬิกาสิบนาที", "สิบสองนาฬิกาห้าสิบนาที", "ยี่สิบนาฬิกาสิบห้านาที", "ยี่สิบสองนาฬิกายี่สิบนาที", "สิบสองนาฬิกาห้าสิบห้านาที", "ห้านาฬิกาสิบห้านาที", "หกนาฬิกาสามสิบนาที", "สิบแปดนาฬิกาสามสิบห้านาที", "สิบเก้านาฬิกาสามสิบห้านาที", "สีี่นาฬิกาสี่สิบนาที", "แปดนาฬิกาห้านาที"};
+    private String[] choice3Mode1Strings = new String[]{"เก้านาฬิกาสิบห้านาที", "แปดนาฬิกาห้านาที", "สิบห้านาฬิกาห้าสิบห้านาที", "สิบเก้านาฬิกาสามสิบนาที", "สิบเจ็ดนาฬิกาสิบห้านาที", "สี่นาฬิกาสามสิบนาที", "แปดนาฬิกาสี่สิบห้านาที", "สิบเแปดนาฬิกาสี่สิบนาที", "ยี่สิบเอ็ดนาฬิกาห้านาที", "สิบเจ็ดนาฬิกาห้าสิบนาที", "เก้านาฬิกาสิบนาที", "สิบนาฬิกาห้าสิบนาที", "ยี่สิบเอ็ดนาฬิกาสิบห้านาที", "ยี่สิบนาฬิกายี่สิบนาที", "สิบสามนาฬิกาห้าสิบห้านาที", "หกนาฬิกาสิบห้านาที", "แปดนาฬิกาสามสิบนาที", "สิบเหกนาฬิกาสามสิบห้านาที", "สิบเก้านาฬิกาสี่สิบห้านาที", "หกนาฬิกาสี่สิบนาที", "หกนาฬิกาห้านาที"};
+    private String[] choice4Mode1Strings = new String[]{"แปดนาฬิกาห้านาที", "เจ็ดนาฬิกาสิบห้านาที", "สิบสองนาฬิกาห้าสิบห้านาที", "สิบเจ็ดนาฬิกาสามสิบนาที", "สิบห้านาฬิกายี่สิบนาที", "ห้านาฬิกาสามสิบห้านาที", "เจ็ดนาฬิกาสี่สิบห้านาที", "สิบเจ็ดนาฬิกาสี่สิบนาที", "ยี่สิบนาฬิกาห้านาที", "สิบหกนาฬิกาห้าสิบนาที", "สิบนาฬิกาสิบนาที", "สิบเอ็ดนาฬิกาห้าสิบนาที", "สิบเก้านาฬิกาสิบห้านาที", "ยี่สิบเอ็ดนาฬิกายี่สิบนาที", "สิบห้านาฬิกาห้าสิบห้านาที", "หกนาฬิกายี่สิบนาที", "ห้านาฬิกาสามสิบนาที", "สิบเจ็ดนาฬิกาสามสิบห้านาที", "สิบแปดนาฬิกาสี่สิบห้านาที", "ห้านาฬิกาสี่สิบนาที", "เก้านาฬิกาห้านาที"};
+//    private String[] choice1Mode1Strings = new String[]{"ABC1"};
+//    private String[] choice2Mode1Strings = new String[]{"ABC1"};
+//    private String[] choice3Mode1Strings = new String[]{"ABC1"};
+//    private String[] choice4Mode1Strings = new String[]{"ABC1"};
+
+                                                                                                                           //7
+    private String[] choice1Mode3Strings = new String[]{"ช้อนตวง"     , "กระบอกตวงน้ำมัน", "ถัง"       , "ลิตร"    , "กระบอกตวง", "ลิตร"       , "    4 ลิตร    ", "    8 ลิตร    ", "    50 ลิตร    ", "    20 ถุง    ", "    14 ลิตร    ", "    6 ลิตร    ", "   10 วัน   ", "    350 ลิตร    ", "    54 ลิตร    ", "    75 ลิตร    ", "    100 ลิตร    ", "    47 ลิตร    ", "    40 ลิตร    ", "    28 ลิตร    ", "    10 กระติก    "};
+    private String[] choice2Mode3Strings = new String[]{"ลิตร"       , "ช้อนตวง"    , "กระบอกตวงน้ำมัน", "กระบอกตวง", "ช้อนตวง"  , "ถัง"        , "    5 ลิตร    ", "    9 ลิตร    ", "    45 ลิตร    ", "    7 ถุง    ", "    12 ลิตร    ", "    8 ลิตร    ", "    9 วัน    ", "    10 ลิตร    ", "    74 ลิตร    ", "    65 ลิตร    ", "    70 ลิตร    ", "    44 ลิตร    ", "    30 ลิตร    ", "    20 ลิตร    ", "    7 กระติก    "};
+    private String[] choice3Mode3Strings = new String[]{"ถัง"        , "ลิตร"      , "ช้อนตวง"    , "ถัง"     , "ถัง"     , "กระบอกตวง"   , "    8 ลิตร    ", "    7 ลิตร    ", "    65 ลิตร    ", "    8 ถุง    ", "    15 ลิตร    ", "    7 ลิตร    ", "    7 วัน    ", "    250 ลิตร    ", "    64 ลิตร    ", "    70 ลิตร    ", "    80 ลิตร    ", "    51 ลิตร    ", "    10 ลิตร    ", "    24 ลิตร    ", "    8 กระติก    "};
+    private String[] choice4Mode3Strings = new String[]{"กระบอกตวงน้ำมัน", "ถัง"       , "กระบอกตวง"  , "กระบอกตวง", "ถ้วยตวง"   , "ถ้วยตวง"     , "    7 ลิตร    " , "    12 ลิตร "  , "    55 ลิตร    ", "    10 ถุง    ", "    16 ลิตร    ", "    9 ลิตร    ", "   8 วัน    ", "    100 ลิตร    ", "    84 ลิตร    ", "    74 ลิตร    ", "ห    90 ลิตร    ", "    41 ลิตร    ", "   20 ลิตร    ", "    14 ลิตร    ", "    9 กระติก    "};
+//    private String[] choice1Mode2Strings = new String[]{"ABC1"};
+//    private String[] choice2Mode2Strings = new String[]{"ABC1"};
+//    private String[] choice3Mode2Strings = new String[]{"ABC1"};
+//    private String[] choice4Mode2Strings = new String[]{"ABC1"};
+                                                                                                                  //6
+    private String[] choice1Mode2Strings = new String[]{" 2 กิโลกรัม 3 ขีด ", " 3 กิโลกรัม", "4 กิโลกรัมฉ", "1 กิโลกรัม", " 5 กิโลกรัม"," 1 กิโลกรัม 5 ขีด ", " 1 กิโลกรัม"," 2 กิโลกรัม", " 3 กิโลกรัม",  "4 กิโลกรัม 5 ขีด", "1 กิโลกรัม 5 ขีด", " 5 กิโลกรัม", "1 กิโลกรัม", "1 กิโลกรัม", " 1 กิโลกรัม    5 ขีด", " 3 กิโลกรัม", "4 กิโลกรัม 5 ขีด", "1 กิโลกรัม ", " 5 กิโลกรัม", "1 กิโลกรัม 5 ขีด", "1 กิโลกรัม","3 กิโลกรัม 5 ขีด"};
+    private String[] choice2Mode2Strings = new String[]{" 3 กิโลกรัม 3 ขีด ", " 2 กิโลกรัม", "5 กิโลกรัม", "3 กิโลกรัม", " 3 กิโลกรัม", " 4 กิโลกรัม 5 ขีด ", " 5 กิโลกรัม"," 3 กิโลกรัม", " 5 กิโลกรัม",  "5 กิโลกรัมิ 5 ขีด", "3 กิโลกรัม 5 ขีด", " 3 กิโลกรัม", " 4 กิโลกรัม ", " 5 กิโลกรัม", " 3 กิโลกรัม 5 ขีด", " 2 กิโลกรัม", "3 กิโลกรัม 5 ขีด", "2 กิโลกรัม  ", " 3 กิโลกรัม", " 3 กิโลกรัม 5 ขีด ", " 3 กิโลกรัม","2 กิโลกรัม 5 ขีด"};
+    private String[] choice3Mode2Strings = new String[]{" 4 กิโลกรัม 3 ขีด ", " 4 กิโลกรัม", "3 กิโลกรัม", "5 กิโลกรัม", " 2 กิโลกรัม", " 3 กิโลกรัม 5 ขีด ", " 4 กิโลกรัม", " 4 กิโลกรัม", "4 กิโลกรัม",  "4 กิโลกรัม 5 ขีด", "5 กิโลกรัม 5 ขีด ", " 1 กิโลกรัม", " 3 กิโลกรัม ", " 4 กิโลกรัม", " 2 กิโลกรัม 5 ขีด", "4 กิโลกรัม", "3 กิโลกรัม 5 ขีด", "5 กิโลกรัม ", " 1 กิโลกรัม", " 2 กิโลกรัม 5 ขีด ", " 4 กิโลกรัม","4 กิโลกรัม 5 ขีด"};
+    private String[] choice4Mode2Strings = new String[]{" 5 กิโลกรัม 3 ขีด ", " 5 กิโลกรัม", "1 กิโลกรัม", "2 กิโลกรัม", " 4 กิโลกรัม", " 5 กิโลกรัม      ", " 2 กิโลกรัม", " 5 กิโลกรัม", " 2 กิโลกรัม", "1 กิโลกรัม 5 ขีด ", "4 กิโลกรัม 5 ขีด", " 4 กิโลกรัม", "5 กิโลกรัม", "3 กิโลกรัม"," 5 กิโลกรัม    5 ขีด", " 5 กิโลกรัม", "1 กิโลกรัม 5 ขีด", "3 กิโลกรัม ", " 4 กิโลกรัม", " 4 กิโลกรัม 5 ขีด", " 2 กิโลกรัม","5 กิโลกรัม 5 ขีด"};
+//    private String[] choice1Mode3Strings = new String[]{"ABC1"};
+//    private String[] choice2Mode3Strings = new String[]{"ABC1"};
+//    private String[] choice3Mode3Strings = new String[]{"ABC1"};
+//    private String[] choice4Mode3Strings = new String[]{"ABC1"};
+
+
+    private int[] eatInts = new int[]{R.drawable.eat1, R.drawable.eat2,
+            R.drawable.eat3, R.drawable.eat4, R.drawable.eat5};
+    private int[] timeDelayInts = new int[]{100, 80, 50};
+
+
+//    All Getter
+
+    public int[] getTimeDelayInts() {
+        return timeDelayInts;
+    }
+
+    public int[] getEatInts() {
+        return eatInts;
+    }
+
+    public int[] getAnswerMode0Ints() {
+        return answerMode0Ints;
+    }
+
+    public int[] getAnswerMode1Ints() {
+        return answerMode1Ints;
+    }
+
+    public int[] getAnswerMode2Ints() {
+        return answerMode2Ints;
+    }
+
+    public int[] getAnswerMode3Ints() {
+        return answerMode3Ints;
+    }
+
+    public String[] getChoice1Mode0Strings() {
+        return choice1Mode0Strings;
+    }
+
+    public String[] getChoice2Mode0Strings() {
+        return choice2Mode0Strings;
+    }
+
+    public String[] getChoice3Mode0Strings() {
+        return choice3Mode0Strings;
+    }
+
+    public String[] getChoice4Mode0Strings() {
+        return choice4Mode0Strings;
+    }
+
+    public String[] getChoice1Mode1Strings() {
+        return choice1Mode1Strings;
+    }
+
+    public String[] getChoice2Mode1Strings() {
+        return choice2Mode1Strings;
+    }
+
+    public String[] getChoice3Mode1Strings() {
+        return choice3Mode1Strings;
+    }
+
+    public String[] getChoice4Mode1Strings() {
+        return choice4Mode1Strings;
+    }
+
+    public String[] getChoice1Mode2Strings() {
+        return choice1Mode2Strings;
+    }
+
+    public String[] getChoice2Mode2Strings() {
+        return choice2Mode2Strings;
+    }
+
+    public String[] getChoice3Mode2Strings() {
+        return choice3Mode2Strings;
+    }
+
+    public String[] getChoice4Mode2Strings() {
+        return choice4Mode2Strings;
+    }
+
+    public String[] getChoice1Mode3Strings() {
+        return choice1Mode3Strings;
+    }
+
+    public String[] getChoice2Mode3Strings() {
+        return choice2Mode3Strings;
+    }
+
+    public String[] getChoice3Mode3Strings() {
+        return choice3Mode3Strings;
+    }
+
+    public String[] getChoice4Mode3Strings() {
+        return choice4Mode3Strings;
+    }
+
+
+    public int[] getModeGame0Ints() {
+        return modeGame0Ints;
+    }
+
+    public int[] getModeGame1Ints() {
+        return modeGame1Ints;
+    }
+
+    public int[] getModeGame2Ints() {
+        return modeGame2Ints;
+    }
+
+    public int[] getModeGame3Ints() {
+        return modeGame3Ints;
+    }
+}// Main Method
